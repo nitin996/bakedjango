416744f30d7d6ad151913934a15d610281be9b42
Nitin <gupta6nitin@gmail.com>
2024-03-07 15:37:48 +0530

    first commit

---
+++ shop/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class category(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(upload_to='category')
+
+    def __str__(self):
+        return self.title
+    
